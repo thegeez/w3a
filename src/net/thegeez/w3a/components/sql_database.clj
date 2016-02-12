b33e9b4bee8a70fc17d1095196ac2ae5b57defbd
thegeez <thegeez@users.noreply.github.com>
2016-02-12 19:24:09 +0100

    JDBC connection string for heroku

--- src/net/thegeez/w3a/components/sql_database.clj
+++ src/net/thegeez/w3a/components/sql_database.clj
@@ -1,9 +1,19 @@
 (ns net.thegeez.w3a.components.sql-database
   (:require [io.pedestal.log :as log]
             [com.stuartsierra.component :as component]
-            [clojure.java.jdbc :as jdbc])
+            [clojure.java.jdbc :as jdbc]
+            [clojure.string :as string])
   (:import [java.net URI]
            [com.mchange.v2.c3p0 ComboPooledDataSource]))
+
+(defn db-url-for-heroku [db-url]
+  (let [db-uri (URI. db-url)
+        host (.getHost db-uri)
+        port (.getPort db-uri)
+        path (.getPath db-uri)
+        [user password] (string/split (.getUserInfo db-uri) #":")]
+    (str "jdbc:postgresql://" host ":" port path "?user=" user "&password=" password "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory")))
+
 
 (defrecord SQLDatabase [db-connect-string]
   component/Lifecycle
